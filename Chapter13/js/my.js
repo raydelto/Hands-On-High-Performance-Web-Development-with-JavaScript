2f0a923254581c3642cbf8752364c2ab240b3dc4
Raydelto Hernandez <raydelto@gmail.com>
2021-07-07 16:49:14 -0300

    Adding more wat test files

--- Chapter13/js/my.js
+++ Chapter13/js/my.js
@@ -2,11 +2,12 @@ console.log("Loading wasm ...");
 
 async function loadWasm()
 {
-    const response = await fetch("useless.wasm");
+    const response = await fetch("math.wasm");
     const buffer = await response.arrayBuffer();
     const obj = await WebAssembly.instantiate(buffer).then((obj)=>{
-        console.log(obj);
+        // console.log(obj);
         console.log("WebAssembly object has been loaded");
+        // console.log(obj.instance.exports.add(100,200));
     });
 
     console.log("WASM has been loaded");
