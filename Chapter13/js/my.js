b179fa42b2bbb5d200d099737d85b8384a63e5dd
Raydelto Hernandez <raydelto@gmail.com>
2021-07-07 10:55:21 -0300

    Adding verbosity to my loadWasm function

--- Chapter13/js/my.js
+++ Chapter13/js/my.js
@@ -4,7 +4,10 @@ async function loadWasm()
 {
     const response = await fetch("useless.wasm");
     const buffer = await response.arrayBuffer();
-    const obj = await WebAssembly.instantiate(buffer);
+    const obj = await WebAssembly.instantiate(buffer).then((obj)=>{
+        console.log(obj);
+        console.log("WebAssembly object has been loaded");
+    });
 
     console.log("WASM has been loaded");
 }
