f6b3dbbf613d67150f05410abd34ffdf4b0bf892
Raydelto Hernandez <raydelto@gmail.com>
2021-07-08 11:09:02 -0300

    Adding the fizzbuzzc Exercise

--- Chapter13/fizzbuzz.c
+++ Chapter13/fizzbuzz.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-void fizzbuzz(int num) {
-    for(int i = 1; i <= num; i++) {
-        if(i%3 == 0) {
+
+void fizzbuzz(int num) 
+{
+    for(int i = 1; i <= num; i++) 
+    {
+        if(i%3 == 0) 
+        {
             printf("Fizz");
         }
-        if(i%5 == 0) {
+        if(i%5 == 0) 
+        {
             printf("Buzz");
         }
     }
